22a4ab2e8ce798e4020db9cd6a97ee40256452fd
marcelpinotti <marcel.pinotti@gmail.com>
2021-12-16 10:01:09 -0300

    Subindo o projeto final no repositório

---
+++ src/main/java/com/bancopan/cloud9/bluebank/config/AwsSnsSmsConfig.java
@@ -0,0 +1,26 @@
+package com.bancopan.cloud9.bluebank.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+
+@Configuration
+public class AwsSnsSmsConfig {
+
+    public static final String SECRET_KEY = "COOLOQUE AQUI SUA SENHA DE IAM AWS";
+    public static final String ACCESS_KEY = "COLOQUE AQUI SUA CHAVE DE ACESSO IAM AWS";
+
+
+    @Bean
+    @Primary
+    public AmazonSNSClient getSnsClient() {
+        return (AmazonSNSClient) AmazonSNSClientBuilder.standard().withRegion(Regions.US_EAST_1)
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY))).build();
+    }
+}
---
+++ src/main/java/com/bancopan/cloud9/bluebank/controllers/ClienteController.java
@@ -0,0 +1,142 @@
+package com.bancopan.cloud9.bluebank.controllers;
+
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.model.SubscribeRequest;
+import com.bancopan.cloud9.bluebank.dtos.*;
+import com.bancopan.cloud9.bluebank.models.ClienteModel;
+import com.bancopan.cloud9.bluebank.repositories.ClienteRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/api/cliente")
+@Api(value = "API REST BLUEBANK")
+@CrossOrigin(origins = "*")
+public class ClienteController {
+
+    @Autowired
+    private ClienteRepository repository;
+
+    @Autowired
+    private AmazonSNSClient snsClient;
+
+    String TOPIC_ARN = "arn:aws:sns:us-east-1:965934840569:Cloud9_sns";
+
+    @GetMapping(value = "/listar")
+    @ApiOperation(value = "Retorna uma lista de todos os clientes")
+    public ResponseEntity<List<ConsultaClienteModelDTO>> getAllClienteModel() {
+        List<ClienteModel> clienteModelList = repository.findAll();
+        List<ConsultaClienteModelDTO> consultaClienteModelDTOList = ConsultaClienteModelDTO.converteParaDTO(clienteModelList);
+        return ResponseEntity.ok(consultaClienteModelDTOList);
+    }
+
+    @GetMapping(value = "/{idCliente}")
+    @ApiOperation(value = "Retorna um cliente pelo id")
+    public ResponseEntity<ConsultaClienteModelDTO> consultarCliente(@PathVariable("idCliente") Long idCliente) {
+
+        if (!repository.existsById(idCliente)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        ClienteModel clienteModel = repository.getById(idCliente);
+        ConsultaClienteModelDTO consultaClienteModelDTO = new ConsultaClienteModelDTO(clienteModel);
+        return ResponseEntity.ok(consultaClienteModelDTO);
+    }
+
+    @GetMapping(value = "/endereco/{idCliente}")
+    @ApiOperation(value = "Retorna o cliente pelo id somente com o endereço")
+    public ResponseEntity<ConsultaClienteEnderecoDTO> consultarClientePorEnderecoDTO(@PathVariable("idCliente") Long idCliente) {
+
+        if (!repository.existsById(idCliente)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        ClienteModel clienteModel = repository.getById(idCliente);
+        ConsultaClienteEnderecoDTO consultaClienteEnderecoDTO = new ConsultaClienteEnderecoDTO(clienteModel);
+        return ResponseEntity.ok(consultaClienteEnderecoDTO);
+    }
+
+    @GetMapping(value = "/contato/{idCliente}")
+    @ApiOperation(value = "Retorna o cliente pelo id somente com os contatos")
+    public ResponseEntity<ConsultaClienteContatoDTO> consultarClientePorContatoDTO(@PathVariable("idCliente") Long idCliente) {
+
+        if (!repository.existsById(idCliente)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        ClienteModel clienteModel = repository.getById(idCliente);
+        ConsultaClienteContatoDTO consultaClienteContatoDTO = new ConsultaClienteContatoDTO(clienteModel);
+        return ResponseEntity.ok(consultaClienteContatoDTO);
+    }
+
+    @GetMapping(value = "/tipo/{tipoCliente}")
+    @ApiOperation(value = "Retorna uma lista de clientes pelo tipo")
+    public ResponseEntity<List<ClienteModel>> filtroPorTipo(@PathVariable("tipoCLiente") String tipoCliente) {
+
+        List<ClienteModel> listaClienteModel = repository.procuraTipoCliente(tipoCliente);
+        return ResponseEntity.ok(listaClienteModel);
+    }
+
+    @GetMapping(value = "/tipo_filtro/{tipoCliente}")
+    @ApiOperation(value = "Retorna uma lista de clientes pelo tipo com endereços e contatos")
+    public ResponseEntity<List<ConsultaClienteTipoDTO>> filtroPorTipoDTO(@PathVariable("tipoCliente") String tipoCliente) {
+
+
+        List<ClienteModel> listaClienteModel = repository.procuraTipoCliente(tipoCliente);
+        List<ConsultaClienteTipoDTO> listaConsultaClienteTipoDTO = ConsultaClienteTipoDTO.converteParaDTO(listaClienteModel);
+        return ResponseEntity.ok(listaConsultaClienteTipoDTO);
+    }
+
+    @PostMapping(value = "/salvar")
+    @ApiOperation(value = "Salva um novo cliente")
+    public ResponseEntity<ClienteModel> salvarCliente(@Valid @RequestBody CriarClienteDTO criarClienteDTO) {
+        SubscribeRequest request = new SubscribeRequest(TOPIC_ARN, "email", criarClienteDTO.getEmail());
+        snsClient.subscribe(request);
+
+        ClienteModel clienteModel = CriarClienteDTO.converteParaModel(criarClienteDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(clienteModel));
+    }
+
+    @DeleteMapping(value = "/deletar/{idCliente}")
+    @ApiOperation(value = "Deleta um cliente")
+    public ResponseEntity<Void> deleteCliente(@PathVariable("idCliente") Long idCliente) {
+
+        if (!repository.existsById(idCliente)) {
+            return ResponseEntity.notFound().build();
+        }
+        repository.deleteById(idCliente);
+        return ResponseEntity.noContent().build();
+
+    }
+
+
+    @Transactional
+    @RequestMapping(value = "/atualizar/{idCliente}", method = RequestMethod.PUT)
+    @ApiOperation(value = "Atualiza os dados de um cliente")
+    public ResponseEntity<ClienteModel> Put(@PathVariable(value = "idCliente") Long idCliente, @Valid @RequestBody ClienteModel cliente) {
+
+        Optional<ClienteModel> clienteModel = repository.findById(idCliente);
+        if (clienteModel.isPresent()) {
+            ClienteModel pessoa = clienteModel.get();
+            pessoa.setNome(cliente.getNome());
+            pessoa.setEmail(cliente.getEmail());
+            pessoa.setTelefone(cliente.getTelefone());
+            pessoa.setEnderecoModel(cliente.getEnderecoModel());
+            cliente.setContaCorrenteModel(cliente.getContaCorrenteModel());
+            pessoa.setRenda(cliente.getRenda());
+            repository.save(pessoa);
+            return new ResponseEntity<>(pessoa, HttpStatus.OK);
+        } else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+}
